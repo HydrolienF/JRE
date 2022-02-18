9799a7d27bae515437e93c338deeaf17f55c5d63
HydrolienF <hydrolien.f@gmail.com>
2022-02-18 11:47:35 +0100

    remove jlink

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-rm -r jlink/jLinux
+rm -fr jlink/
+mkdir jlink/
+# mkdir jlink/jLinux
+# mkdir jlink/jMac
+# mkdir jlink/jWindows
 jlink --module-path "jmods/linux" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jLinux"
-rm -r jlink/jMac
 jlink --module-path "jmods/mac" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jMac"
-rm -r jlink/jWindows
 jlink --module-path "jmods/windows" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jWindows"
