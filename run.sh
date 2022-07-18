d5a32a96d327eb51a56333b70ee9e8985491a3ac
HydrolienF <hydrolien.f@gmail.com>
2022-07-18 17:25:01 +0200

    remove unuse part

--- run.sh
+++ run.sh
@@ -3,6 +3,3 @@ mkdir jlink/
 jlink --module-path "jmods/linux" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jLinux"
 jlink --module-path "jmods/mac" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jMac"
 jlink --module-path "jmods/windows" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jWindows"
-chmod 700 jlink/jLinux/bin/java
-chmod 700 jlink/jMac/bin/java
-ls -la jlink/jLinux/bin/
