dcfaa48c13665025b8a3a09597cfb9f9686f6fe7
HydrolienF <hydrolien.f@gmail.com>
2022-11-06 11:08:33 +0100

    Add module jdk.unsupported

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm -fr jlink/
 mkdir jlink/
-jlink --module-path "jmods/linux" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jLinux"
-jlink --module-path "jmods/mac" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jMac"
-jlink --module-path "jmods/windows" --add-modules "java.desktop,java.base,jdk.crypto.ec" --output "jlink/jWindows"
+jlink --module-path "jmods/linux" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported" --output "jlink/jLinux"
+jlink --module-path "jmods/mac" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported" --output "jlink/jMac"
+jlink --module-path "jmods/windows" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported" --output "jlink/jWindows"
