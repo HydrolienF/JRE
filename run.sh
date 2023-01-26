cf4d5a88d52a9360a2b8b59a1eb08c7b01f9eadf
HydrolienF <hydrolien.f@gmail.com>
2023-01-26 13:08:08 +0100

    Add logger

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm -fr jlink/
 mkdir jlink/    
-jlink --module-path "jmods/linux" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported" --output "jlink/jLinux"
-jlink --module-path "jmods/mac" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported" --output "jlink/jMac"
-jlink --module-path "jmods/windows" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported" --output "jlink/jWindows"
+jlink --module-path "jmods/linux" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported,java.logging" --output "jlink/jLinux"
+jlink --module-path "jmods/mac" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported,java.logging" --output "jlink/jMac"
+jlink --module-path "jmods/windows" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported,java.logging" --output "jlink/jWindows"
