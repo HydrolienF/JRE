a9a999fd9d6b4d40488e409c852a0ccaa98cabf8
HydrolienF <hydrolien.f@gmail.com>
2023-01-26 14:25:02 +0100

    new run with java.sql

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-rm -fr jlink/
 mkdir jlink/    
-jlink --module-path "jmods/linux" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported,java.logging" --output "jlink/jLinux"
-jlink --module-path "jmods/mac" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported,java.logging" --output "jlink/jMac"
-jlink --module-path "jmods/windows" --add-modules "java.desktop,java.base,jdk.crypto.ec,jdk.unsupported,java.logging" --output "jlink/jWindows"
+MODULE='"java.desktop,java.base,jdk.crypto.ec,jdk.unsupported,java.logging,java.sql"'
+jlink --module-path "jmods/linux" --add-modules $MODULE --output "jlink/jLinux"
+jlink --module-path "jmods/mac" --add-modules $MODULE --output "jlink/jMac"
+jlink --module-path "jmods/windows" --add-modules $MODULE --output "jlink/jWindows"
